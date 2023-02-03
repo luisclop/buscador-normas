1382e0622e99c98a2fa5269efe5928ea4c667cdd
Luis Lopez <luiskar22@gmail.com>
2023-02-02 22:18:49 -0500

    Agrega base de TablaResultados

--- src/components/TablaResultados.tsx
+++ src/components/TablaResultados.tsx
@@ -6,11 +6,27 @@ export function TablaResultados() {
     <div>
       <Table>
         <Table.Head>
-          <Table.HeadCell>Product name</Table.HeadCell>
-          <Table.HeadCell>Color</Table.HeadCell>
-          <Table.HeadCell>Category</Table.HeadCell>
-          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>Sector</Table.HeadCell>
+          <Table.HeadCell>Norma</Table.HeadCell>
+          <Table.HeadCell>Sumilla</Table.HeadCell>
         </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>PRODUCE</Table.Cell>
+            <Table.Cell>Norma 1</Table.Cell>
+            <Table.Cell>Sumilla 1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>PRODUCE</Table.Cell>
+            <Table.Cell>Norma 2</Table.Cell>
+            <Table.Cell>Sumilla 2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>PRODUCE</Table.Cell>
+            <Table.Cell>Norma 3</Table.Cell>
+            <Table.Cell>Sumilla 3</Table.Cell>
+          </Table.Row>
+        </Table.Body>
       </Table>
     </div>
   );
